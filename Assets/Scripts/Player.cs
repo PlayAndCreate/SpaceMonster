949c1bb08772096dd3fe85226181cee24a5134af
sazarina <sazarina100@gmail.com>
2017-03-29 16:43:55 +0900

    自機の移動

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    float kSpeed = 3.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,25 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float x = Input.GetAxisRaw("Horizontal") * 1.7f;
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(x, y).normalized;
+        Move(direction);
+    }
+
+    void Move(Vector2 direction) {
+        // 画面左下のワールド座標をビューポートから取得
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        // 画面右上のワールド座標をビューポートから取得
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(3, 1));
+        // プレイヤーの座標を取得
+        Vector2 pos = transform.position;
+        // 移動量を加える
+        pos += direction * kSpeed * Time.deltaTime;
+        // プレイヤーの位置が画面内に収まるように制限をかける
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+        pos.y = Mathf.Clamp(pos.y, min.y, max.y);
+        // 制限をかけた値をプレイヤーの位置とする
+        transform.position = pos;
+    }
 }
