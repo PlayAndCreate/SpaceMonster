0f251d72adf67a30b599563351fb4a7e7e3ef667
sazarina <sazarina100@gmail.com>
2017-04-11 01:04:03 +0900

    usingの整理とupdate()の削除など
    csvデータから敵データを読み込むコードを書きました

--- Assets/Scripts/EnemyDataLoad.cs
+++ Assets/Scripts/EnemyDataLoad.cs
@@ -1,16 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.IO;
+using System.Text;
 
 public class EnemyDataLoad : MonoBehaviour {
+    const int kEnemyAll = 4;//敵のすべての数
+    public bool isDebug = false;
+    public bool isLoad = true;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject enemyController;
+    EnemyController enemyControll;
+
+    // Use this for initialization
+    void Start() {
+        enemyControll = enemyController.GetComponent<EnemyController>();
+        if (isLoad) {
+            Load("enemyData");
+        } else {
+            Save(@"Assets/Resources/enemyData.csv");
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    bool Save(string filePath) {
+        FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite);
+        Encoding shiftJis = Encoding.GetEncoding("SHIFT_JIS");
+        StreamWriter sw = new StreamWriter(fs, shiftJis);
+        sw.Close();
+        return true;
+    }
+
+    bool Load(string filePath) {
+        string EnemyCSV = ((TextAsset)Resources.Load(filePath, typeof(TextAsset))).text;
+
+        string[] EnemyList = EnemyCSV.Split('\n');
+        var header = EnemyList[0].Split(',');
+        for (int i = 0; i < kEnemyAll; i++) {
+            var EnemyDataCol = EnemyList[i + 1].Split(',');//ヘッダーを飛ばす
+            //Debug.LogWarningFormat("{0}",EnemyDataCol);
+            string name = EnemyDataCol[0];
+            enemyControll.Freq[name] = int.Parse(EnemyDataCol[1]);
+            enemyControll.Attack[name] = int.Parse(EnemyDataCol[2]);
+        }
+        return true;
+    }
 }
