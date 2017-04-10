ff9e2a6a7230f0fc0658f351b684343ea6add4ab
sazarina <sazarina100@gmail.com>
2017-04-11 01:06:19 +0900

    連装配列を使って敵出現頻度と攻撃力をそこにいれる

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
+    Player player;
+    //敵出現頻度と攻撃力の連想配列
+    public Dictionary<string, int> Freq = new Dictionary<string, int>();
+    public Dictionary<string, int> Attack = new Dictionary<string, int>();
+
+    public void AddEnemyData(string name, int freq, int attack) {
+        Freq.Add(name, freq);
+        Attack.Add(name, attack);
+    }
+
     // Use this for initialization
     void Start() {
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
 
+        AddEnemyData("zako1", 80, 20);
+
+        foreach (KeyValuePair<string, int> pair in Freq) {
+            Debug.Log(pair.Key + " freq is " + pair.Value);
+        }
     }
 
     // Update is called once per frame
