f70ac2da825cb4062c3a96901484d6a5b5ce54e6
sazarina <sazarina100@gmail.com>
2017-04-16 16:58:31 +0900

    行数も表示するようにしました

--- Assets/Scripts/EnemyDataLoad.cs
+++ Assets/Scripts/EnemyDataLoad.cs
@@ -35,9 +35,9 @@ public class EnemyDataLoad : MonoBehaviour {
         string[] EnemyList = EnemyCSV.Split('\n');
         var header = EnemyList[0].Split(',');
 
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 43; i++) {
             var EnemyDataCol = EnemyList[i + 1].Split(',');//ヘッダーを飛ばす
-            Debug.LogWarningFormat("{0}",EnemyDataCol);
+            Debug.LogWarningFormat(i+":{0}",EnemyDataCol);
             string name = EnemyDataCol[0];
             //enemyControll.Freq[name] = int.Parse(EnemyDataCol[1]);
             //enemyControll.Attack[name] = int.Parse(EnemyDataCol[2]);
